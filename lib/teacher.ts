016f39f04087d5bd21829a68120d31a2ba5ae138
mmarthaksimmons <mmarthaksimmons@gmail.com>
2025-01-13 07:18:35 +0800

    Update teacher.ts

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,7 +1,6 @@
 export function isTeacher(userId?: string | null) {
-  if (!userId || !process.env.NEXT_PUBLIC_TEACHER_ID) return false;
-  
+  if (!userId || !process.env.NEXT_PUBLIC_TEACHER_ID) return false
   // Split the teacher IDs string into an array and check if userId is included
-  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_ID.split(',').map(id => id.trim());
-  return teacherIds.includes(userId);
+  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_ID.split(',').map(id => id.trim())
+  return teacherIds.includes(userId)
 }
