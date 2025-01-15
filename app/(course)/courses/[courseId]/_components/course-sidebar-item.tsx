46cc46f6d7b9204ac5a313d46b303496b2891132
mmarthaksimmons <mmarthaksimmons@gmail.com>
2025-01-15 10:23:18 +0800

    Update course-sidebar-item.tsx

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -29,15 +29,16 @@ export default function CourseSidebarItem({ id, label, isCompleted, courseId, is
       onClick={onClick}
       type="button"
       className={cn(
-        'flex items-center gap-x-2 pl-6 text-sm font-medium text-slate-500 transition-all hover:bg-slate-300/20 hover:text-slate-600',
+        'flex items-center gap-x-2 pl-6 text-sm font-medium transition-all hover:text-slate-600',
         {
-          'bg-slate-200/20 text-slate-700 hover:bg-slate-200/20 hover:text-slate-700': isActive,
-          'text-emerald-700 hover:text-emerald-700': isCompleted,
-          'bg-emerald-200/20 ': isCompleted && isActive,
+          'text-slate-500 hover:bg-slate-300/20': !isActive && !isCompleted,
+          'bg-slate-200/20 text-slate-700 hover:bg-slate-200/20 hover:text-slate-700': isActive && !isCompleted,
+          'text-emerald-700 hover:text-emerald-700 hover:bg-emerald-300/20': isCompleted && !isActive,
+          'bg-emerald-200/20 text-emerald-700 hover:bg-emerald-200/20 hover:text-emerald-700': isCompleted && isActive,
         },
       )}
     >
-      <div className="flex items-center gap-x-2 py-4">
+      <div className="flex items-center gap-x-2 py-4 rounded-md">
         <Icon
           size={22}
           className={cn('text-slate-500', { 'text-slate-700': isActive, 'text-emerald-700': isCompleted })}
@@ -46,9 +47,10 @@ export default function CourseSidebarItem({ id, label, isCompleted, courseId, is
       </div>
 
       <div
-        className={cn('ml-auto h-full border-2 border-slate-700 opacity-0 transition-all', {
+        className={cn('ml-auto h-full w-1 border-r-2 border-slate-700 opacity-0 transition-all', {
           'opacity-100': isActive,
-          'border-emerald-700': isCompleted,
+          'border-emerald-700': isCompleted && !isActive,
+          'border-slate-700': isActive,
         })}
       />
     </button>
