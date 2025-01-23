b2c31088657f464c1d4b02374a0bdf05e1e1945b
mmarthaksimmons <mmarthaksimmons@gmail.com>
2025-01-23 12:55:00 +0800

    Update format.ts

--- lib/format.ts
+++ lib/format.ts
@@ -2,5 +2,7 @@ export function formatPrice(price: number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'MYR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(price)
 }
