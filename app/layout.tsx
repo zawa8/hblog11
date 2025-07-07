1ffed57a150f6a849aa54c46bd644ad6c09a070c
viml kumar <vimalkg@gmail.com>
2025-07-07 06:42:20 +0530

    hscii.vercel.app fbse.vercel.app fb.com/ztrabc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+//import { Inter } from 'next/font/google'
+import { inglishenglosoftw8asc } from "@/components/hsciifp/hsciifonts";
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers'
 
-const inter = Inter({ subsets: ['latin'] })
+//const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'LMS (Learning Management System)',
@@ -16,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={inglishenglosoftw8asc.className}>
           <ConfettiProvider />
           <ToastProvider />
           {children}
